85817efc3e9a9948fed28bca5037fc24d08c93e6
Avni Goel <avnigoel98@gmail.com>
2021-12-31 01:13:07 +0530

    Sign Out Done

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
 const express = require("express");
-const {signup , signin} = require("../controllers/auth");
+const {signup , signin , signout} = require("../controllers/auth");
 const {userSignUpValidator} = require("../validators/index");
 
 const router = express.Router();
 
 router.post("/signup" ,userSignUpValidator, signup);
 router.post("/signin" ,signin);
+router.get("/signout" ,signout);
+
 
 module.exports = router;
