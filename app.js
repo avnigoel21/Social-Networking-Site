c6f46042f7bdca214040ed7037fe23d63b352238
Avni Goel <avnigoel98@gmail.com>
2022-01-02 10:42:50 +0530

    added apiDocs

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser")
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
+const fs = require("fs");
 const expressValidator = require ('express-validator');
 dotenv.config();
 
@@ -20,6 +21,18 @@ mongoose.connection.on("error" , err => {
 const postRoutes =require("./routes/post");
 const authRoutes =require("./routes/auth");
 const userRoutes =require("./routes/user");
+//apiDocs -  yhi route bna diya
+app.get('/' , (req , res) => {
+    fs.readFile('docs/apiDocs.json' , (err , data) =>{
+        if(err) {
+            res.status(400).json({
+                error: err
+            });
+        }
+        const docs = JSON.parse(data);
+        res.json(docs); 
+    });
+});
 
 //middleware
 app.use(morgan("dev"));
