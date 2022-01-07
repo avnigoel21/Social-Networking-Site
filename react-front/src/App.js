f2469acbe4e3d7cf9b2521d0fbb2334b260fe67f
Avni Goel <avnigoel98@gmail.com>
2022-01-07 21:30:22 +0530

    SignIn SignUp done

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import MainRouter from './core/MainRouter';
+import MainRouter from './MainRouter';
 
 const App = () => (
   <BrowserRouter>
--- react-front/src/user/Signup.js
+++ react-front/src/user/Signup.js
@@ -1,13 +1,112 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Signup extends Component {
-    render() { 
+    constructor() {
+        super();
+        this.state = {
+            name: "",
+            email: "",
+            password: "",
+            error: "",
+            open: false
+        };
+    }
+
+    handleChange = name => event => {
+        this.setState({ error: "" });
+        this.setState({ [name]: event.target.value });
+    }
+
+
+    clickSubmit = event => {
+        event.preventDefault()
+        const { name, email, password } = this.state
+        const user = {
+            name,
+            email,
+            password
+        };
+
+        //console.log(user);
+        this.signup(user).then(data => {
+            if (data.error) this.setState({ error: data.error })
+            else this.setState({
+                error: "",
+                name: "",
+                email: "",
+                password: "",
+                open: true
+            });
+        });
+    };
+
+    signup = user => {
+        return fetch("http://localhost:8080/signup", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+            .then(response => {
+                return response.json()
+            })
+            .catch(err => console.log(err))
+    };
+
+    render() {
+        const { name, email, password, error, open } = this.state;
         return (
-        <div>
-            <h2>Signup</h2>
-        </div>
+            <div className='container'>
+                <h2>SIGN UP</h2>
+                <div className='alert alert-danger'
+                    style={{ display: error ? "" : "none" }}
+                >
+                    {error}
+                </div>
+                <div
+                    className='alert alert-info'
+                    style={{ display: open ? "" : "none" }}
+                >
+                    New account is successfully created. Please Sign in.
+                </div>
+
+                <form>
+                    <div className="form-group">
+                        <label className="text-muted">Name</label>
+                        <input
+                            onChange={this.handleChange("name")}
+                            type="text"
+                            className="form-control"
+                            value={name}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label className="text-muted">Email</label>
+                        <input
+                            onChange={this.handleChange("email")}
+                            type="email"
+                            className="form-control"
+                            value={email}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label className="text-muted">Password</label>
+                        <input
+                            onChange={this.handleChange("password")}
+                            type="password"
+                            className="form-control"
+                            value={password}
+                        />
+                    </div>
+                    <button onClick={this.clickSubmit} type="submit" className="btn btn-primary" >
+                        Submit
+                    </button>
+                </form>
+            </div>
         );
     }
 }
- 
+
 export default Signup;
