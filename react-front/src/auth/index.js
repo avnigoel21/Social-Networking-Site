b335620d40ad462f65083309848ac4bf354c8a45
Avni Goel <avnigoel98@gmail.com>
2022-01-08 13:43:20 +0530

    change in index.js

--- react-front/src/auth/index.js
+++ react-front/src/auth/index.js
@@ -1,5 +1,5 @@
  export const signup = user => {
-    return fetch(`$process.env.REACT_APP_API_URL}/signup`, {
+    return fetch(`${process.env.REACT_APP_API_URL}/signup`, {
         method: "POST",
         headers: {
             Accept: "application/json",
@@ -14,7 +14,7 @@
 };
 
 export const signin = user => {
-    return fetch(`$process.env.REACT_APP_API_URL}/signin`, {
+    return fetch(`${process.env.REACT_APP_API_URL}/signin`, {
         method: "POST",
         headers: {
             Accept: "application/json",
@@ -38,7 +38,7 @@ export const authenticate = (jwt, next) => {
 export const signout = (next) => {
     if (typeof window !== "undefined") localStorage.removeItem("jwt")
     next()
-    return fetch(`$process.env.REACT_APP_API_URL}/signout`, {
+    return fetch(`${process.env.REACT_APP_API_URL}/signout`, {
         method: "GET"
     })
         .then(response => {
